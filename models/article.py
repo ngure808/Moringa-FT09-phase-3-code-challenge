0028a9cfbeb3ce440261212a26c1eaa47f9e4d07
ngure808 <mwangingure808@gmail.com>
2024-06-10 16:10:14 +0300

    complete lab

--- models/article.py
+++ models/article.py
@@ -1,10 +1,89 @@
-class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+from database.connection import get_db_connection
 
-    def __repr__(self):
-        return f'<Article {self.title}>'
+
+
+class Article:
+    def __init__(self, title, content, author, magazine):
+        self._id = None
+        self._title = self.validate_title(title)
+        self._content = self.validate_content(content)
+        self._author = author
+        self._magazine = magazine
+        self.save()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def author_id(self):
+        return self._author.id
+
+    @property
+    def magazine_id(self):
+        return self._magazine.id
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
+                       (self._title, self._content, self._author.id, self._magazine.id))
+        conn.commit()
+        self._id = cursor.lastrowid
+
+    @staticmethod
+    def validate_title(title):
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string")
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        return title
+
+    @staticmethod
+    def validate_content(content):
+        if not isinstance(content, str):
+            raise ValueError("Content must be a string")
+        return content
+
+    @property
+    def author(self):
+        from models.author import Author
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT authors.id, authors.name
+            FROM authors
+            JOIN articles ON authors.id = articles.author_id
+            WHERE articles.id = ?
+        ''', (self.id,))
+        row = cursor.fetchone()
+        return Author(row["id"], row["name"]) if row else None
+
+    @property
+    def magazine(self):
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT magazines.id, magazines.name, magazines.category
+            FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
+            WHERE articles.id = ?
+        ''', (self.id,))
+        row = cursor.fetchone()
+        return Magazine(row["id"], row["name"], row["category"]) if row else None
+
+    @classmethod
+    def drop_table(cls):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE IF EXISTS articles')
+        conn.commit()
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,112 @@
-class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+from database.connection import get_db_connection
 
-    def __repr__(self):
-        return f'<Magazine {self.name}>'
+class Magazine:
+    def __init__(self, name, category):
+        self._id = None
+        self._name = self.validate_name(name)
+        self._category = self.validate_category(category)
+        self.save()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (self._name, self._category))
+        conn.commit()
+        self._id = cursor.lastrowid
+
+    @staticmethod
+    def validate_name(name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        return name
+
+    @staticmethod
+    def validate_category(category):
+        if not isinstance(category, str):
+            raise ValueError("Category must be a string")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        return category
+
+    def articles(self):
+        from models.article import Article
+        from models.author import Author
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT articles.id, articles.title, articles.content, articles.author_id, articles.magazine_id
+            FROM articles
+            WHERE articles.magazine_id = ?
+        ''', (self.id,))
+        rows = cursor.fetchall()
+        return [Article(row["id"], row["title"], row["content"], Author(row["author_id"], ""), self) for row in rows]
+
+    def contributors(self):
+        from models.author import Author
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT DISTINCT authors.id, authors.name
+            FROM authors
+            JOIN articles ON authors.id = articles.author_id
+            WHERE articles.magazine_id = ?
+        ''', (self.id,))
+        rows = cursor.fetchall()
+        return [Author(row["id"], row["name"]) for row in rows]
+
+    @classmethod
+    def drop_table(cls):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE IF EXISTS magazines')
+        conn.commit()
+
+    def article_titles(self):
+        # Fetch articles associated with the magazine
+        articles = self.articles()
+        # If no articles found, return None
+        if not articles:
+            return None
+        # Extract titles from articles
+        titles = [article.title for article in articles]
+        return titles
+
+    def contributing_authors(self):
+        from models.author import Author
+        # Fetch articles associated with the magazine
+        articles = self.articles()
+        # If no articles found, return None
+        if not articles:
+            return None
+        # Count the number of articles written by each author
+        author_counts = {}
+        for article in articles:
+            if article.author_id in author_counts:
+                author_counts[article.author_id] += 1
+            else:
+                author_counts[article.author_id] = 1
+        # Filter authors who have written more than 2 articles
+        contributing_authors = []
+        for author_id, count in author_counts.items():
+            if count > 2:
+                author = Author.find_by_id(author_id)
+                if author:
+                    contributing_authors.append(author)
+        # If no authors found, return None
+        if not contributing_authors:
+            return None
+        return contributing_authors
